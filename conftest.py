529508d9a08c3088dbac633c6a7426e9f057072e
Vadim Korolev <vkorol98@gmail.com>
2024-06-12 19:59:00 +0300

    fixture to different file

---
+++ conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from selene import browser, be, have
+from selenium import webdriver
+
+@pytest.fixture(scope="function", autouse=True)
+def browser_management():
+    browser.config.base_url = 'https://google.com/'
+    browser.config.timeout = 10
+    browser.config.driver_name = 'firefox'
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = "eager"
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
