ad70d21a036ba35f7f0b6856da51472c154c1ccc
Vadim Korolev <vkorol98@gmail.com>
2024-06-12 22:34:41 +0300

    fix

--- conftest.py
+++ conftest.py
@@ -6,9 +6,11 @@ from selenium import webdriver
 @pytest.fixture(scope="function", autouse=True)
 def browser_management():
     browser.config.base_url = 'https://google.com/'
-    browser.config.timeout = 10
     browser.config.driver_name = 'firefox'
-    driver_options = webdriver.ChromeOptions()
+    driver_options = webdriver.FirefoxOptions()
     driver_options.page_load_strategy = "eager"
+    browser.config.driver_options = driver_options
     browser.config.window_height = 1080
     browser.config.window_width = 1920
+    yield
+    browser.quit()
--- test_google_search.py
+++ test_google_search.py
@@ -4,10 +4,10 @@ from selene import browser, be, have
 def test_search_pass():
     browser.open('')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+    browser.element('#search').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
 def test_search_incorrect():
     browser.open('')
     browser.element('[name="q"]').should(be.blank).type('ysertdjsfgjksfgjdfgjdfhksdtk').press_enter()
-    browser.element('[id="botstuff"]').should(have.text('По запросу ysertdjsfgjksfgjdfgjdfhksdtk ничего не найдено.'))
+    browser.element('#botstuff').should(have.text('По запросу ysertdjsfgjksfgjdfgjdfhksdtk ничего не найдено.'))
